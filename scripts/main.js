d9b4c2bef498129e256a11bc3158dd7c64026504
Trevor Fischer <tcfischer2@mail.fhsu.edu>
2021-11-04 20:50:59 -0500

    Assignment 7

--- scripts/main.js
+++ scripts/main.js
@@ -1,146 +1,82 @@
-/* Practice Assignment 6:
+/* Practice Assignment 7:
+
 Complete this javascript file according to the individual instructions
 given in the comments. 
+
 *** DO NOT CHANGE any of the code that you are not instructed to. */
 
-// 1) Create an object named webDev that cannot be reassigned and
-// contains the 3 key-value pairs:
-// frame: "HTML", style: "CSS", logic: "JavaScript"
-const webDev = {
-    frame: "HTML",
-    style: "CSS",
-    logic: "JavaScript"
+// 1) Define a function named getTheTable that returns the table
+// element in the HTML document that has the id "myTable".
+function getTheTable(){
+   return document.getElementById('myTable');
 }
-
-// 2) Create an object named pizza with a property named crust.
-// Set the crust value to "thin". Add a method named setCrust
-// to the object that accepts a parameter named crustType.
-// The method should change the crust value of the object to
-// the value of the parameter.
-// Hint: keyword to use is "this".
-let pizza = {
-    crust: "thin",
-    setCrust:function(crustType){
-        this.crust = crustType;
-    }
+// 2) Define a function named getAllParagraphs that returns a
+// NodeList containing all paragraph nodes inside the "myPage"
+// section element of the HTML document.
+function getAllParagraphs(){
+    pList = document.querySelectorAll(document.getElementsByName('myPage'));
+    return pList;
 }
-
-// 3) Create a function named doesItContain that accepts two
-// parameters: "key" and "obj". The function should return
-// boolean data to indicate if the key exists in the object.
-function doesItContain(key, obj){
-    return key in obj;
-}
-
-
-// 4) Create a function named introduceTheActors that accepts
-// the array of objects named actors (see below) as a parameter.
-// Name the parameter "actorArray". The function should loop
-// through the actors array and create the following statement
-// for each actor:
-// "Hi, I'm {actorName} and I was in {movieName}."
-////////
-// Example: "Hi, I'm Keanu and I was in The Matrix."
-////////
-// Store each statement in an array that is returned when
-// the function is complete. The returned array should have
-// all of the created statements inside.
-const actors = [
-    { name: "Keanu", movie: "The Matrix" },
-    { name: "Amy", movie: "Arrival" },
-    { name: "Leonardo", movie: "The Wolf of Wall Street" },
-    { name: "Sigourney", movie: "Alien" },
-    { name: "Jeff", movie: "The Big Lebowski" },
-  ];
-  ///////// Do not change the above array of objects /////////
-
-  function introduceTheActors(actors){
-      let arr = [];
-      for(i=0; i<actors.length;i++ ){
-          arr.push("Hi I'm " + actors[i].name + " and I was in " + actors[i].movie + ".")
-      }
-      return arr;
-  }
-
-  // 5) Create an object named techCompanies that has the following
-  // key-value pairs:
-  // Microsoft: "Bill Gates", Amazon: "Jeff Bezos", Tesla: "Elon Musk",
-  // Facebook: "Mark Zuckerberg", Apple: "Steve Jobs"
-  // Add a method to the techCompanies object named founderLookup
-  // that accepts companyName as a parameter and returns
-  // the name of the founder.
-
-  let techCompanies = {
-    Microsoft: "Bill Gates", 
-    Amazon: "Jeff Bezos", 
-    Tesla: "Elon Musk", 
-    Facebook: "Mark Zuckerberg", 
-    Apple: "Steve Jobs",
-    founderLookup: function(companyName){
-        if(companyName in this){
-            return this[companyName];
-        }
-        else{
-            return null;
+// 3) Define a function named makeItalic.
+// The function should change the CSS style of
+// the paragraph elements with the class "even"
+// to have italicized text. The function should
+// return a NodeList containing the paragraphs
+// that were changed to italics.
+/////////////
+// Hint: You will need a loop.
+// Also reference CSS in JavaScript.
+function makeItalic(){
+    pList = getAllParagraphs();
+    newList;
+    for(i = 0; i < length(pList); i ++){
+        if (pList[i].classList.contains('even')){
+            pList[i].style.fontStyle = 'italic';
+            newList.push(pList[i]);
         }
     }
-  }
- 
-  
-  // 6) Utlizing the same techCompanies object from #5,
-  // Create a function named storeFounders that accepts
-  // an object and loops through the values. Store the
-  // name of each founder in a new array that is returned
-  // by the function when the loop is complete.
-  function storeFounders(obj){
-      let arr = Object.values(obj);
-      return arr;
-  }
-  console.log(storeFounders(techCompanies));
-  
-  // 7) Create a function named goToSecondClass that accepts
-  // a destructured object as a parameter. The parameter
-  // extracts the "secondHour" property of the object.
-  // The function should return this statement:
-  // "Time to go to {property_value} class!"
-  //////////
-  // Example: "Time to go to Programming class!"
-  /////////
-  // Test your function with this object:
-  const myClasses = {
-    firstHour: "Ethics",
-    secondHour: "Programming",
-    thirdHour: "Biology",
-  };
-  ////////// Don't change the above object
+    return newList;
+}
 
-  function goToSecondClass(second){
-      return "Time to go to " + second + " class!"
-  }
-  
-  // 8a) Create a generic object named pie.
-  // Give the pie object a property named "slices"
-  // that has a value of 8. Give the pie object
-  // a method named "taste". Have the taste method
-  // return "Wow!".
-  /////
+// 4) Define a function named createBorders that selects
+// the table inside the "myPage" section. The function
+// should add a CSS border to the table cells that is
+// 2 pixels thick and solid black (use #000 for black).
+// The function should also return the table element.
+////////////
+// Hint: You can build a selector from the result
+// of another selector.
+function createBorders(){
+   table = getTheTable();
+   table.style.createBorders = "2px solid #000";
+   return table;
+}
 
-  const pie = {
-      slices: 8,
-      taste: function(){
-          return "Wow!";
-      }
-  }
-  // 8b) Create a new object named "blueBerryPie"
-  // that uses the pie object as a constructor
-  // to inherit its property and method. Use
-  // dot notation to add a flavor property to
-  // the "blueBerryPie" object that has a value of
-  // "blueberry". Also, update the taste method of
-  // this new object to return "Delicious!"
-  ////////////////
-  // Hint: Inheritance is in this week's video.
+// 5) Define a function named getTheListItem that accepts two
+// number parameters. The first parameter (num1) is the number of
+// the unordered list in the document. The second parameter (num2)
+// is the number of the list item in the unordered list.
+// The function should return the text content of the
+// requested list item.
+/////////////////////////
+// For example: getTheListItem(2,2) should return
+// second list item of the second unordered list inside the
+// "myPage" section element of the HTML document.
+////////////////////////
+// Hint: MDN for nth-of-type and nth-child selectors
+function getTheListItem(num1, num2){
+    page = document.getElementsByName('myPage');
+    list = page.querySelectorAll('ul');
+    return list[num1][num2];
+}
 
-  const blueBerryPie = Object.create(pie);
-  blueBerryPie.flavor = "blueberry";
-  blueBerryPie.taste = "Delicious";
+// 6) Define a function named changeBgColor that changes the
+// background color of the "myPage" section. The function
+// should accept one parameter named "color" that will be
+// the color the function changes the background to.
+// The function should return the "myPage" section element.
+function changeBgColor(color){
+    page = document.getElementsByName('myPage');
+    document.page.style.backgroundColor = color;
+    return page;
+}
